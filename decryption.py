51480da70a004a4979761d21fccf983306eae98a
peuch <peu.chen7@gmail.com>
2023-04-03 22:57:05 -0400

    Update decryption.py
    Added decryption class

--- decryption.py
+++ decryption.py
@@ -1,2 +1,11 @@
-
-
+class Decryption:
+    def __init__(self):
+        return
+    def InvSubBytes():
+        return 1
+    def InvShiftRows():
+        return 1
+    def InvMixColumns():
+        return 1
+    def InvAddRoundKeys():
+        return 1
