d389be1e49547bbf31432b9ed993255d60f331a1
David Calderon <dcalderon32@gatech.edu>
2023-04-03 22:50:12 -0400

    Added basic functions

--- encryption.py
+++ encryption.py
@@ -1,2 +1,11 @@
-
-
+class Encryption:
+    def __init__(self):
+        return
+    def SubBytes():
+        return 1
+    def ShiftRows():
+        return 1
+    def MixColumns():
+        return 1
+    def AddRoundKeys():
+        return 1
