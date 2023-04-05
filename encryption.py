10f6aa59a9971506c7923315c6f6d515cdf9b336
peuch <peu.chen7@gmail.com>
2023-04-05 09:40:43 -0400

    Added MixColumns method

--- encryption.py
+++ encryption.py
@@ -1,3 +1,5 @@
+import galois
+
 class Encryption:
     def __init__(self):
         return
@@ -5,7 +7,18 @@ class Encryption:
         return 1
     def ShiftRows(self):
         return 1
-    def MixColumns(self):
-        return 1
+    
+    def MixColumns(state):
+        for i in range(4):
+            a = state[0, i]
+            b = state[1, i]
+            c = state[2, i]
+            d = state[3, i]
+            state[0, i] = galois.gAdd(galois.gMult(a, 0x02), galois.gAdd(galois.gMult(b, 0x03), galois.gAdd(c, d)))
+            state[1, i] = galois.gAdd(galois.gMult(b, 0x02), galois.gAdd(galois.gMult(c, 0x03), galois.gAdd(a, d)))
+            state[2, i] = galois.gAdd(galois.gMult(c, 0x02), galois.gAdd(galois.gMult(d, 0x03), galois.gAdd(a, b)))
+            state[3, i] = galois.gAdd(galois.gMult(d, 0x02), galois.gAdd(galois.gMult(a, 0x03), galois.gAdd(b, c)))
+        return state
+    
     def AddRoundKeys(self):
         return 1
