1f5ae46ea9478f6b0046c6e06a7441dbad8fb8fc
peuch <peu.chen7@gmail.com>
2023-04-05 09:54:05 -0400

    Added simple wikipedia test to InvMixColumns

--- test_unittest.py
+++ test_unittest.py
@@ -1,5 +1,6 @@
 import galois
 import encryption
+import decryption
 import unittest 
 
 class TestGaloisField(unittest.TestCase):
@@ -12,7 +13,7 @@ class TestGaloisField(unittest.TestCase):
         self.assertEqual(galois.gMult(0x57, 0x83), 0xc1)
     
 class TestMixColumns(unittest.TestCase):
-    def test_1(self):
+    def test_MixColumns_WikipediaTest(self):
         state = [
             [0xdb, 0xf2, 0x01, 0xc6],
             [0x13, 0x0a, 0x01, 0xc6],
@@ -26,6 +27,21 @@ class TestMixColumns(unittest.TestCase):
             [0xbc, 0x9d, 0x01, 0xc6]
         ]
         self.assertEqual(encryption.Encryption.MixColumns(state), expected)
+
+    def test_InvMixColumns_WikipediaTest(self):
+        state = [
+            [0x8e, 0x9f, 0x01, 0xc6],
+            [0x4d, 0xdc, 0x01, 0xc6],
+            [0xa1, 0x58, 0x01, 0xc6],
+            [0xbc, 0x9d, 0x01, 0xc6]
+        ]
+        expected = [
+            [0xdb, 0xf2, 0x01, 0xc6],
+            [0x13, 0x0a, 0x01, 0xc6],
+            [0x53, 0x22, 0x01, 0xc6],
+            [0x45, 0x5c, 0x01, 0xc6]
+        ]
+        self.assertEqual(decryption.Decryption.InvMixColumns(state), expected)
 
 if __name__ == '__main__':
     unittest.main()
