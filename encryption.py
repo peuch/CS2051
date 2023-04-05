9c2c3b7cd96d43b1440518f81cdde0d754c95cd3
peuch <peu.chen7@gmail.com>
2023-04-05 09:47:17 -0400

    Added tests to MixColumns and fixed syntax error

--- encryption.py
+++ encryption.py
@@ -9,15 +9,16 @@ class Encryption:
         return 1
     
     def MixColumns(state):
+        print(state)
         for i in range(4):
-            a = state[0, i]
-            b = state[1, i]
-            c = state[2, i]
-            d = state[3, i]
-            state[0, i] = galois.gAdd(galois.gMult(a, 0x02), galois.gAdd(galois.gMult(b, 0x03), galois.gAdd(c, d)))
-            state[1, i] = galois.gAdd(galois.gMult(b, 0x02), galois.gAdd(galois.gMult(c, 0x03), galois.gAdd(a, d)))
-            state[2, i] = galois.gAdd(galois.gMult(c, 0x02), galois.gAdd(galois.gMult(d, 0x03), galois.gAdd(a, b)))
-            state[3, i] = galois.gAdd(galois.gMult(d, 0x02), galois.gAdd(galois.gMult(a, 0x03), galois.gAdd(b, c)))
+            a = state[0][i]
+            b = state[1][i]
+            c = state[2][i]
+            d = state[3][i]
+            state[0][i] = galois.gAdd(galois.gMult(a, 0x02), galois.gAdd(galois.gMult(b, 0x03), galois.gAdd(c, d)))
+            state[1][i] = galois.gAdd(galois.gMult(b, 0x02), galois.gAdd(galois.gMult(c, 0x03), galois.gAdd(a, d)))
+            state[2][i] = galois.gAdd(galois.gMult(c, 0x02), galois.gAdd(galois.gMult(d, 0x03), galois.gAdd(a, b)))
+            state[3][i] = galois.gAdd(galois.gMult(d, 0x02), galois.gAdd(galois.gMult(a, 0x03), galois.gAdd(b, c)))
         return state
     
     def AddRoundKeys(self):
--- test_unittest.py
+++ test_unittest.py
@@ -1,4 +1,5 @@
 import galois
+import encryption
 import unittest 
 
 class TestGaloisField(unittest.TestCase):
@@ -10,5 +11,21 @@ class TestGaloisField(unittest.TestCase):
         self.assertEqual(galois.gMult(0x57, 0x13), 0xfe)
         self.assertEqual(galois.gMult(0x57, 0x83), 0xc1)
     
+class TestMixColumns(unittest.TestCase):
+    def test_1(self):
+        state = [
+            [0xdb, 0xf2, 0x01, 0xc6],
+            [0x13, 0x0a, 0x01, 0xc6],
+            [0x53, 0x22, 0x01, 0xc6],
+            [0x45, 0x5c, 0x01, 0xc6]
+        ]
+        expected = [
+            [0x8e, 0x9f, 0x01, 0xc6],
+            [0x4d, 0xdc, 0x01, 0xc6],
+            [0xa1, 0x58, 0x01, 0xc6],
+            [0xbc, 0x9d, 0x01, 0xc6]
+        ]
+        self.assertEqual(encryption.Encryption.MixColumns(state), expected)
+
 if __name__ == '__main__':
     unittest.main()
