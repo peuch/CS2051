a0e1e559ae19b213f9741cbfac5eedb0ecd69c54
peuch <peu.chen7@gmail.com>
2023-04-05 14:07:20 -0400

    Addded InvAddRoundKeys method

--- decryption.py
+++ decryption.py
@@ -8,4 +8,25 @@ class Decryption:
     def InvMixColumns():
         return 1
     def InvAddRoundKeys():
-        return 1
+        ret = [[0 for _ in range(4)] for _ in range(4)]
+        for i in range(4):
+            auxKey = (key // (256**i)) % 256
+
+            a = state[0][i]
+            b = state[1][i]
+            c = state[2][i]
+            d = state[3][i]
+
+            aux = a * 256**3 + b * 256**2 + c * 256 + d
+
+            aux ^= auxKey
+
+            ret[3][i] = aux % 256
+            aux //= 256
+            ret[2][i] = aux % 256
+            aux //= 256
+            ret[1][i] = aux % 256
+            aux //= 256
+            ret[0][i] = aux % 256
+
+        return ret
