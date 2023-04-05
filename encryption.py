447837dbf8a103350d470c38e6cf615ec0c649d8
reesewang25 <reesewang10@gmail.com>
2023-04-05 15:42:06 -0400

    implement shiftrows

--- encryption.py
+++ encryption.py
@@ -3,8 +3,13 @@ class Encryption:
         return
     def SubBytes(self):
         return 1
-    def ShiftRows(self):
-        return 1
+    def ShiftRows(state):
+        ret = [[0 for _ in range(4)] for _ in range(4)]
+        for i in range(4):
+            thisrow=state[i].copy()
+            for j in range(4):
+                ret[i][j]=thisrow[(j+i)%4]
+        return ret
     def MixColumns(self):
         return 1
     def AddRoundKeys(self):
