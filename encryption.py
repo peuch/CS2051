d412435e480fa4f1aee5acbcecbd2b6a1e5f5a0d
David Calderon <dcalderon32@gatech.edu>
2023-04-06 22:10:51 -0400

    Added SubBytes

--- encryption.py
+++ encryption.py
@@ -1,8 +1,14 @@
 class Encryption:
     def __init__(self):
         return
-    def SubBytes(self):
-        return 1
+    def SubBytes(ciphertext: [int][int], key: [int][int]):
+        for r in range(key):
+            for c in range(key[r]):
+                item: int = key[r][c]
+                cipher_row: int = item / 16
+                cipher_col: int = item % 16
+                key[r][c] = ciphertext[cipher_row][cipher_col]
+        return key
     def ShiftRows(self):
         return 1
     def MixColumns(self):
